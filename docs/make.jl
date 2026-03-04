2e5c821b09f3dba2209c1465f800573d67a5e107
Jeff Paril <jeffersonparil@gmail.com>
2026-03-04 20:49:00 +1100

    setting up docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,20 @@
 using CropPhenoFromDrones
 using Documenter
 
-DocMeta.setdocmeta!(CropPhenoFromDrones, :DocTestSetup, :(using CropPhenoFromDrones); recursive=true)
+DocMeta.setdocmeta!(CropPhenoFromDrones, :DocTestSetup, :(using CropPhenoFromDrones); recursive = true)
 
 makedocs(;
-    modules=[CropPhenoFromDrones],
-    authors="jeffersonparil@gmail.com",
-    sitename="CropPhenoFromDrones.jl",
-    format=Documenter.HTML(;
-        canonical="https://CropPhenoFromDrones.github.io/CropPhenoFromDrones.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [CropPhenoFromDrones],
+    authors = "jeffersonparil@gmail.com",
+    sitename = "CropPhenoFromDrones.jl",
+    format = Documenter.HTML(;
+        canonical = "https://jeffersonfparil.github.io/CropPhenoFromDrones.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
+    doctest = false,
+    checkdocs = :exports,
 )
 
-deploydocs(;
-    repo="github.com/CropPhenoFromDrones/CropPhenoFromDrones.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/jeffersonfparil/CropPhenoFromDrones.jl", devbranch = "main")
